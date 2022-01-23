8f6182f156f082afb4d351f54f9a109bf422e257
pal4de <pal4de@gmail.com>
2022-01-23 09:55:52 +0000

    コード中に秘密鍵を保持しないように

--- test/IoTMicropayment.js
+++ test/IoTMicropayment.js
@@ -1,47 +1,46 @@
 const IoTMicropayment = artifacts.require("IoTMicropayment");
 
-contract("IoTMicropayment", () => {
-    describe("支払い", async () => {
-        let instance, buyer, seller;
-
+contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
+    describe("System", async () => {
         before(async () => {
             instance = await IoTMicropayment.deployed();
-
-            const buyerPrivateKey = "0xa7cc8232ebd84cb2e7ec6548247958349245f2f7fa5ae8932ee685c3e4e337cc";
-            const sellerPrivateKey = "0x68612f49e208e4520576b8d7c4a3a0a92099eff5ff0293c8783303a13eba92de";
-            web3.eth.accounts.wallet.add(buyerPrivateKey);
-            web3.eth.accounts.wallet.add(sellerPrivateKey);
-            buyer = web3.eth.accounts.wallet[0];
-            seller = web3.eth.accounts.wallet[1];
         });
 
-        it("署名の検証", async () => {
+        it("支払い", async () => {
             const actBuyer = async () => {
-                const amount = 50;
-                const nonce = 1;
+                const fixSignature = (original) => {
+                    const constant = 0x1bn;
+                    const added = (BigInt(original) + constant);
+                    return "0x" + added.toString(16).padStart(130, "0");
+                }
 
-                const hash = web3.utils.soliditySha3(
-                    { t: "address", v: buyer.address },
-                    { t: "uint256", v: amount },
-                    { t: "uint256", v: nonce },
-                    { t: "address", v: instance.address }
-                ).toString("hex");
-                const { signature } = await buyer.sign(hash);
-                console.log(signature);
-                // const signature  = await web3.eth.sign(hash, buyer.address);
+                const makeSignature = async (amount, nonce) => {
+                    const hash = web3.utils.soliditySha3(
+                        { t: "address", v: buyer },
+                        { t: "uint256", v: amount },
+                        { t: "uint256", v: nonce },
+                        { t: "address", v: instance.address }
+                    ).toString("hex");
+                    return fixSignature(await web3.eth.sign(hash, buyer));
+                }
+
+                const amount = 50;
+                const nonce = 2;
 
                 const params = [amount, nonce];
+                const signature = await makeSignature(amount, nonce);
+
                 return [params, signature];
             }
 
             const actSeller = async (params, signature) => {
-                const options = { from: seller.address }
+                const options = { from: seller }
                 const result = await instance.claimPayment(...params, signature, options);
                 return result;
             }
 
-            assert.equal(buyer.address, await instance.buyer());
-            assert.equal(seller.address, await instance.seller());
+            assert.equal(await instance.buyer(), buyer);
+            assert.equal(await instance.seller(), seller);
 
             const [params, signature] = await actBuyer();
             await actSeller(params, signature);
