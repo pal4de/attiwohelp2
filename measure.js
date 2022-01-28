1a806bc2ae2c0ca47aea465db71d54f1c9e401b8
pal4de <pal4de@gmail.com>
2022-01-28 10:49:57 +0000

    csvクラスを自作

--- measure.js
+++ measure.js
@@ -19,6 +19,36 @@ class Parameter {
   }
 }
 
+class Csv {
+  constructor(name, header) {
+    this.length = header.length;
+    this.name = name;
+    this.body = header.join() + "\n";
+  }
+
+  add(...values) {
+    if (values.length != this.length) {
+      throw new Error("カラムの長さが一致しません");
+    }
+    this.body += values.join() + "\n";
+  }
+
+  writeOut() {
+    const writer = fs.createWriteStream(`result/${this.name}.csv`);
+    writer.write(iconv.encode(this.body, "Shift_JIS"));
+    writer.end();
+  }
+}
+
+const withProgress = (count, f) => {
+  return async () => {
+    const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    progress.start(count, 0);
+    await f(() => progress.increment());
+    progress.stop();
+  }
+}
+
 const ethInYen = 277852; // 2022年1月24日 12:20現在 BitFlyerにて
 
 const BN = (x) => new web3.utils.BN(x);
@@ -26,12 +56,6 @@ const BN = (x) => new web3.utils.BN(x);
 const balanceOf = async (address) => BigInt(await web3.eth.getBalance(address));
 
 const weiToYen = (wei) => parseFloat(web3.utils.fromWei(`${wei}`, "ether")) * ethInYen;
-
-const writeOut = (content, name) => {
-  const writer = fs.createWriteStream(`result/${name}.csv`);
-  writer.write(iconv.encode(content, "Shift_JIS"));
-  writer.end();
-}
 
 contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
   describe("System", async () => {
@@ -89,161 +113,95 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
       return [expected, actual, txFee];
     }
 
-    it("amountと手数料の関係", async () => {
-      const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-      progress.start(50, 0);
-
-      let csv = "販売個数, 引き出し金額, 販売者の残高の変動量, 取引手数料\n";
+    it("amountと手数料の関係", withProgress(50, async (increment) => {
+      const csv = new Csv("amount", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
       for (let amount = 1; amount <= 100; amount += 2) {
         const instance = await newInstance();
         const params = { nonce: 1, amount };
         const [expected, actual, txFee] = await tx(instance, params);
 
-        const row = [
-          amount,
-          weiToYen(expected),
-          weiToYen(actual),
-          weiToYen(txFee),
-        ]
-        csv += `${row.join(", ")}\n`;
-
-        progress.increment();
+        csv.add(amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
       }
-      writeOut(csv, "amount");
-      progress.stop();
-    });
+      csv.writeOut();
+    }));
 
-    it("nonceと手数料の関係", async () => {
-      const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-      progress.start(50, 0);
-
-      let csv = "ナンス, 引き出し金額, 販売者の残高の変動量, 取引手数料\n";
+    it("nonceと手数料の関係", withProgress(50, async (increment) => {
+      const csv = new Csv("nonce", ["ナンス", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
       for (let nonce = 1; nonce <= 100; nonce += 2) {
         const instance = await newInstance();
         const params = { nonce, amount: 1 };
         const [expected, actual, txFee] = await tx(instance, params);
 
-        const row = [
-          nonce,
-          weiToYen(expected),
-          weiToYen(actual),
-          weiToYen(txFee),
-        ]
-        csv += `${row.join(", ")}\n`;
-
-        progress.increment();
+        csv.add(nonce, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
       }
-      writeOut(csv, "nonce");
-      progress.stop();
-    });
+      csv.writeOut();
+    }));
 
-    it("単価と手数料の関係", async () => {
-      const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-      progress.start(50, 0);
-
-      let csv = "単価, 引き出し金額, 販売者の残高の変動量, 取引手数料\n";
+    it("単価と手数料の関係", withProgress(50, async (increment) => {
+      const csv = new Csv("unitPrice", ["単価", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
       for (let unitPrice = 0; unitPrice < 50; unitPrice += 1) {
         const instance = await newInstance();
         const params = { nonce: 1, amount: 1 };
         const [expected, actual, txFee] = await tx(instance, params);
 
-        const row = [
-          unitPrice,
-          weiToYen(expected),
-          weiToYen(actual),
-          weiToYen(txFee),
-        ]
-        csv += `${row.join(", ")}\n`;
-
-        progress.increment();
+        csv.add(unitPrice, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
       }
-      writeOut(csv, "unitPrice");
-      progress.stop();
-    });
+      csv.writeOut();
+    }));
 
-    it("継続的な取引と手数料の関係 (1つずつ)", async () => {
-      const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-      progress.start(50, 0);
-
+    it("継続的な取引と手数料の関係 (1つずつ)", withProgress(50, async (increment) => {
       const instance = await newInstance();
       const parameter = new Parameter();
 
-      let csv = "販売個数, 引き出し金額, 販売者の残高の変動量, 取引手数料\n";
+      const csv = new Csv("sequential-01", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
       for (let nonce = 1; nonce <= 100; nonce += 2) {
         const params = parameter.gen(2);
         const [expected, actual, txFee] = await tx(instance, params, 1);
 
-        const row = [
-          params.amount,
-          weiToYen(expected),
-          weiToYen(actual),
-          weiToYen(txFee),
-        ]
-        csv += `${row.join(", ")}\n`;
-
-        progress.increment();
+        csv.add(params.amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
       }
-      writeOut(csv, "sequential-01");
-      progress.stop();
-    });
+      csv.writeOut();
+    }));
 
-    it("継続的な取引と手数料の関係 (5つずつ)", async () => {
-      const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-      progress.start(20, 0);
-
+    it("継続的な取引と手数料の関係 (5つずつ)", withProgress(20, async (increment) => {
       const instance = await newInstance();
       const parameter = new Parameter();
 
-      let csv = "販売個数, 引き出し金額, 販売者の残高の変動量, 取引手数料\n";
+      const csv = new Csv("sequential-05", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
       for (let nonce = 1; nonce <= 20; nonce++) {
         const params = parameter.gen(5);
         const [expected, actual, txFee] = await tx(instance, params, 5);
 
-        const row = [
-          params.amount,
-          weiToYen(expected),
-          weiToYen(actual),
-          weiToYen(txFee),
-        ]
-        csv += `${row.join(", ")}\n`;
-
-        progress.increment();
+        csv.add(params.amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
       }
-      writeOut(csv, "sequential-05");
-      progress.stop();
-    });
+      csv.writeOut();
+    }));
 
-    it("継続的な取引と手数料の関係 (10ずつ)", async () => {
-      const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-      progress.start(10, 0);
-
+    it("継続的な取引と手数料の関係 (10ずつ)", withProgress(10, async (increment) => {
       const instance = await newInstance();
       const parameter = new Parameter();
 
-      let csv = "販売個数, 引き出し金額, 販売者の残高の変動量, 取引手数料\n";
+      const csv = new Csv("sequential-10", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
       for (let nonce = 1; nonce <= 10; nonce++) {
         const params = parameter.gen(10);
         const [expected, actual, txFee] = await tx(instance, params, 10);
 
-        const row = [
-          params.amount,
-          weiToYen(expected),
-          weiToYen(actual),
-          weiToYen(txFee),
-        ]
-        csv += `${row.join(", ")}\n`;
-
-        progress.increment();
+        csv.add(params.amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
       }
-      writeOut(csv, "sequential-10");
-      progress.stop();
-    });
+      csv.writeOut();
+    }));
 
     it("単価と損益分岐点の関係", async () => {
       const multibar = new cliProgress.MultiBar({}, cliProgress.Presets.shades_classic);
       const unitPriceProgress = multibar.create(51, 1);
 
-      let csv = "単価, 販売個数\n";
+      const csv = new Csv("surplus", ["単価", "販売個数"]);
       for (let unitPrice = 0.1; unitPrice <= 5; unitPrice += 0.1) {
         const upperLimit = Math.floor(50 / unitPrice);
         const amountProgress = multibar.create(upperLimit, 1);
@@ -254,8 +212,7 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
           const [expected, actual, txFee] = await tx(instance, params);
 
           if (actual > 0) {
-            const row = [unitPrice, amount];
-            csv += `${row.join(", ")}\n`;
+            csv.add(unitPrice, amount);
             break;
           } else {
             amountProgress.update(amount);
@@ -266,7 +223,7 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
         multibar.remove(amountProgress);
         unitPriceProgress.increment();
       }
-      writeOut(csv, "surplus");
+      csv.writeOut();
       multibar.stop();
     });
   });
