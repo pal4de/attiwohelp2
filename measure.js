2ff62e83f0d32b49d3d768845d68b993e0f286bc
pal4de <pal4de@gmail.com>
2022-02-01 05:49:27 +0000

    ガス代を適正化・単価を1円に

--- measure.js
+++ measure.js
@@ -49,7 +49,8 @@ const withProgress = (count, f) => {
   }
 }
 
-const ethInYen = 277852; // 2022年1月24日 12:20現在 BitFlyerにて
+const ethInYen = 315620; // 2022年2月1日14:30現在 BitFlyerにて
+const gasPrice = 112; // 2022年2月1日14:30現在 ETH GAS STATIONにて SAFE LOWを選択
 
 const BN = (x) => new web3.utils.BN(x);
 
@@ -59,7 +60,7 @@ const weiToYen = (wei) => parseFloat(web3.utils.fromWei(`${wei}`, "ether")) * et
 
 contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
   describe("System", async () => {
-    const newInstance = async (unitPriceInYen = 0.5) => {
+    const newInstance = async (unitPriceInYen = 1) => {
       const calcUnitPrice = (ethInYen_, unitPriceInYen_) => {
         const unitPriceInMilliYen = BN(unitPriceInYen_ * 1000);
 
@@ -72,7 +73,7 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
 
       const unitPrice = calcUnitPrice(ethInYen, unitPriceInYen);
       const timeout = 0;
-      const options = { value: unitPrice.mul(BN(10000)) };
+      const options = { value: unitPrice.mul(BN(100000)) };
 
       return await IoTMicropayment.new(
         seller,
@@ -95,7 +96,7 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
     const actSeller = async (instance, { amount, nonce }, signature) => {
       const options = {
         from: seller,
-        gasPrice: web3.utils.toWei("1", "gwei"),
+        gasPrice: web3.utils.toWei(`${gasPrice}`, "gwei"),
       };
       const result = await instance.claimPayment(amount, nonce, signature, options);
       return result;
@@ -113,12 +114,31 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
       return [expected, actual, txFee];
     }
 
+    let roughSurplusAmount, standardSurplusAmount;
+
     it("amountと手数料の関係", withProgress(50, async (increment) => {
       const csv = new Csv("amount", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
-      for (let amount = 1; amount <= 100; amount += 2) {
+      for (let amount = 200; amount <= 10000; amount += 200) {
         const instance = await newInstance();
         const params = { nonce: 1, amount };
         const [expected, actual, txFee] = await tx(instance, params);
+
+        if (actual > 0 && !roughSurplusAmount) roughSurplusAmount = amount - 200;
+
+        csv.add(amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
+        increment();
+      }
+      csv.writeOut();
+    }));
+
+    it("amountと手数料の関係 (細かく)", withProgress(200, async (increment) => {
+      const csv = new Csv("amount-detail", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
+      for (let amount = roughSurplusAmount; amount <= roughSurplusAmount + 200; amount += 1) {
+        const instance = await newInstance();
+        const params = { nonce: 1, amount };
+        const [expected, actual, txFee] = await tx(instance, params);
+
+        if (actual > 0 && !standardSurplusAmount) standardSurplusAmount = amount;
 
         csv.add(amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
         increment();
@@ -128,7 +148,7 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
 
     it("nonceと手数料の関係", withProgress(50, async (increment) => {
       const csv = new Csv("nonce", ["ナンス", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
-      for (let nonce = 1; nonce <= 100; nonce += 2) {
+      for (let nonce = 200; nonce <= 10000; nonce += 200) {
         const instance = await newInstance();
         const params = { nonce, amount: 1 };
         const [expected, actual, txFee] = await tx(instance, params);
@@ -141,7 +161,7 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
 
     it("単価と手数料の関係", withProgress(50, async (increment) => {
       const csv = new Csv("unitPrice", ["単価", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
-      for (let unitPrice = 0.1; unitPrice <= 5; unitPrice += 1) {
+      for (let unitPrice = 0.1; unitPrice <= 5; unitPrice += 0.1) {
         const instance = await newInstance(unitPrice);
         const params = { nonce: 1, amount: 1 };
         const [expected, actual, txFee] = await tx(instance, params);
@@ -157,8 +177,8 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
       const parameter = new Parameter();
 
       const csv = new Csv("sequential", ["販売個数", "引き出し金額", "販売者の残高の変動量", "取引手数料"]);
-      for (let nonce = 1; nonce <= 50; nonce += 1) {
-        const params = parameter.gen(1);
+      for (let nonce = 200; nonce <= 10000; nonce += 200) {
+        const params = parameter.gen(200);
         const [expected, actual, txFee] = await tx(instance, params, 1);
 
         csv.add(params.amount, weiToYen(expected), weiToYen(actual), weiToYen(txFee));
@@ -173,10 +193,25 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
 
       const csv = new Csv("surplus", ["単価", "販売個数"]);
       for (let unitPrice = 0.1; unitPrice <= 5; unitPrice += 0.1) {
-        const upperLimit = Math.floor(50 / unitPrice);
+        const upperLimit = Math.floor(standardSurplusAmount * 1.1 / unitPrice);
         const amountProgress = multibar.create(upperLimit, 1);
 
-        for (let amount = 1; amount <= upperLimit; amount++) {
+        let roughAmount = 0;
+        for (let amount = upperLimit; amount >= 0; amount -= 100) {
+          const instance = await newInstance(unitPrice);
+          const params = { amount, nonce: amount };
+          const [expected, actual, txFee] = await tx(instance, params);
+
+          if (actual < 0) {
+            roughAmount = amount;
+            break;
+          } else {
+            amountProgress.update(amount);
+            if (amount < 0) assert.fail();
+          }
+        }
+
+        for (let amount = roughAmount; amount <= upperLimit; amount++) {
           const instance = await newInstance(unitPrice);
           const params = { amount, nonce: amount };
           const [expected, actual, txFee] = await tx(instance, params);
@@ -186,11 +221,11 @@ contract("IoTMicropayment", ([buyer, seller, ...accounts]) => {
             break;
           } else {
             amountProgress.update(amount);
-            if (amount == upperLimit) assert.failed();
+            if (amount == upperLimit) assert.fail();
           }
         }
-
         multibar.remove(amountProgress);
+
         unitPriceProgress.increment();
       }
       csv.writeOut();
